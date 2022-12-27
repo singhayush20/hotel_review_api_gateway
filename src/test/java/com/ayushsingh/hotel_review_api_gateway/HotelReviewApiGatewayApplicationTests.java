1760ab2a6577f5e40ccdfb047c42e2511db50a95
singhayush20 <ayushsingh20november@gmail.com>
2022-12-28 00:47:48 +0530

    first commit

---
+++ src/test/java/com/ayushsingh/hotel_review_api_gateway/HotelReviewApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayushsingh.hotel_review_api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelReviewApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
